a650f1174e5283fec7243d8497a0db1dcaf14a5b
ql24 <qlalberta@gmail.com>
2017-06-24 18:38:56 -0700

    finished removing duplicates between groups and cleaned the code

--- js/app.js
+++ js/app.js
@@ -4,14 +4,14 @@
 var imageName =
 ['bag.jpg', 'banana.jpg','bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg','cthulhu.jpg', 'dog-duck.jpg','dragon.jpg','pen.jpg','pet-sweep.jpg','scissors.jpg','shark.jpg', 'sweep.png','tauntaun.jpg','unicorn.jpg','usb.gif','water-can.jpg','wine-glass.jpg'];
 var productName = ['Bag', 'Banana slicer','Bathroom table stand', 'Boots', 'Breakfast maker', 'Bubble gum', 'Chair','Cthulhu figure', 'Duck muzzle','Dragon meat','Pen','Pet sweeper','Pizza scissors','Shark sleeping bag', 'Baby sweeper','Tauntaun sleeping bag','Unicorn meat','Tenticle USB','Self watering can','Wine glass'];
-var currentRandomImagePathList = [];
-var currentRandomProductNameList = [];
+var randomImagePathList = [];
+var randomProductNameList = [];
+var previousIndexList = [];
 var indexList = [];
-// var index = 0;
 var attempts = 0;
 var maxAttempts = 25;
 var trial = 25;
-var numberOfImage = 75;
+var numberOfImage = 78;
 var pickList = [];
 var randomProductShownList = [];
 var timesShown = new Array(20).fill(0);
@@ -19,59 +19,23 @@ var timesClicked = new Array(20).fill(0);
 
 //generate random numbers without duplicates
 function generateRandomProductID () {
+  previousIndexList = indexList;
   indexList = [];
-  currentRandomImagePathList = [];
-  currentRandomProductNameList = [];
   for (var i = 0; i < 3; i++) {
     do {
-    var index = Math.floor(Math.random() * imageName.length);
-    } while (indexList.includes(index));
+      var index = Math.floor(Math.random() * imageName.length);
+    } while (indexList.includes(index) || previousIndexList.includes(index));
     indexList.push(index);
   }
 }
 
- function generateRandomProduct () {
-   for (var j = 0; j < 3; j++) {
-    currentRandomImagePathList[j] = imageName[indexList[j]];
-    currentRandomProductNameList[j] = imageName[indexList[j]].slice(0,imageName[indexList[j]].length - 4);
+//generate random products
+function generateRandomProduct () {
+  for (var j = 0; j < 3; j++) {
+    randomImagePathList[j] = imageName[indexList[j]];
+    randomProductNameList[j] = productName[indexList[j]];
   }
 }
-//create a function to check the replicates between the current and the next group
-// function checkReplicates (replicate){
-//   if (currentRandomImageList.includes(replicate)) {
-//     return true;
-//   } else if (previousRandomImageList.includes(replicate)) {
-//     return true;
-//   }
-//   return false;
-// }
-
-//create a function to generate random productNameList
-function removeReplicates () {
-  var previousRandomImagePathList = currentRandomImagePathList;
-  var previousRandomProductNameList = currentRandomProductNameList;
-  // var currentRandomProductNameList = [];
-  // var currentRandomImagePathList = [];
-  for (var ii = 0; ii < 3; ii++) {
-    while (previousRandomImagePathList.includes(currentRandomImagePathList(indexList[ii]))) {
-        for (var jj = 0; jj < 3; jj++) {
-           var index = Math.floor(Math.random() * imageName.length);
-         currentRandomImagePathList[jj] = imageName[indexList[i]].slice(0,imageName[indexList[i]].length - 4);
-       }
-     }
-}
-
-
-
-// // function to generate a random product from the productArray
-// function generateProduct(){
-//   var repeat = doesRepeat(productArrayRef[index]);
-//   while (repeat) {
-//     index = Math.floor(Math.random() * productArrayRef.length);
-//     repeat = doesRepeat(productArrayRef[index]);
-//   }
-//   return productArrayRef[index];
-// }
 
 //get the nodes to attach the selection results
 var productNameParent = document.getElementById('productName');
@@ -92,11 +56,11 @@ function renderProduct () {
 
   for (var m = 0; m < 3; m ++) {
     var h3 = document.createElement('h3');
-    h3.textContent = currentRandomProductNameList[m];
+    h3.textContent = randomProductNameList[m];
     productNameParent.append(h3);
     var img = document.createElement('img');
-    img.setAttribute('src', 'img/assets/' + currentRandomImagePathList[m]);
-    img.setAttribute('id', currentRandomProductNameList[m]);
+    img.setAttribute('src', 'img/assets/' + randomImagePathList[m]);
+    img.setAttribute('id', randomProductNameList[m]);
     productImagesParent.append(img);
   }
 }
@@ -104,7 +68,7 @@ function renderProduct () {
 
 //create function to count the times of images shown
 function timesImageShown () {
-  randomProductShownList = randomProductShownList.concat(currentRandomProductNameList);
+  randomProductShownList = randomProductShownList.concat(randomProductNameList);
   if (randomProductShownList.length == numberOfImage) {
     for (var n = 0; n < randomProductShownList.length; n++) {
       for (var p = 0; p < productName.length; p++) {
@@ -152,7 +116,7 @@ function barChart () {
   var canvas = document.getElementById('barchart');
   var ctx = canvas.getContext('2d');
   // modeled after the Getting Started example in the chartJS docs
-  var barChart = new Chart(ctx, {
+  barChart = new Chart(ctx, {
     type: 'bar',
     data: {
       labels: productName,
@@ -185,7 +149,7 @@ function lineChart () {
   var canvas = document.getElementById('linechart');
   var ctx = canvas.getContext('2d');
   // modeled after the Getting Started example in the chartJS docs
-  var lineChart = new Chart(ctx, {
+  lineChart = new Chart(ctx, {
     type: 'line',
     data: {
       labels: productName,
